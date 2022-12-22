69cbf9ea3d65dc33544e89cf6e8ab9284b9c367b
IuxMorti <zdracte21@bk.ru>
2022-12-22 16:13:00 +0500

    done 1-6

--- index.js
+++ index.js
@@ -1,33 +1,79 @@
-const FORM_HTML = document.getElementsByClassName("beverage").item(0).outerHTML;
-const REMOVE_BUTTONS = document.getElementsByClassName(
-    "delete-beverage-button"
-);
+const b = document.querySelector(".beverage");
+const buttonAdd = document.querySelector(".add-button").parentElement;
+const form = document.querySelector("form");
+const overlay = document.querySelector(".modal-overlay");
 
-const beverages = document.getElementsByClassName("beverage");
-
-let buttonAdd = document
-    .getElementsByClassName("add-button")
-    .item(0).parentElement;
+function openCloseModal() {
+    overlay.classList.toggle("hide");
+}
 
 document
-    .getElementsByClassName("add-button")
-    .item(0)
+    .querySelector(".add-button")
     .addEventListener("click", () => addBeverage());
+document
+    .querySelector(".close-overlay")
+    .addEventListener("click", () => openCloseModal());
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    for (var a of e.target) {
+        if (a.tagName === "FIELDSET") {
+            console.log(a.children);
+        }
+    }
+    console.log(new FormData(form));
+    openCloseModal();
+});
+
+class Beverages {
+    static BeverageElement = document.querySelector(".beverage");
+    static Counter = 1;
+
+    static GetNextBeverageElement() {
+        this.Counter += 1;
+        const bvg = this.BeverageElement.cloneNode(true);
+
+        bvg.querySelector(
+            ".beverage-count"
+        ).innerHTML = `Напиток №${this.Counter}`;
+
+        this.configAttribute(bvg);
+        addRemoveButton(bvg.querySelector(".delete-beverage-button"), bvg);
+
+        return bvg;
+    }
+
+    static configAttribute(element) {
+        if (element?.attributes && element?.attributes?.name) {
+            element.setAttribute(
+                "name",
+                element.attributes["name"].value + `-${this.Counter}`
+            );
+            console.log(element.attributes["name"]);
+        }
+        for (let child of element.childNodes) {
+            this.configAttribute(child);
+        }
+    }
+}
 
 function addBeverage() {
-    let form = FORM_HTML.replace(
-        /(name=)("\S*")/g,
-        (match, p1, p2) => `${p1}${p2.slice(0, -1) + `-${beverages.length}"`}`
+    buttonAdd.insertAdjacentElement(
+        "beforebegin",
+        Beverages.GetNextBeverageElement()
     );
-    buttonAdd.insertAdjacentHTML("beforebegin", form);
-    document
-        .getElementsByClassName("beverage-count")
-        .item(beverages.length - 1).innerHTML = `Напиток №${beverages.length}`;
-
-    let button = REMOVE_BUTTONS.item(beverages.length - 1);
-    addRemoveButton(button, button.parentElement.parentElement);
 }
 
 function addRemoveButton(element, elementRemove) {
     element.addEventListener("click", () => elementRemove.remove());
+}
+
+function declination(number) {
+    if (number % 100 > 10 && number % 100 < 20) return "напитков";
+    else if (number % 10 === 1) {
+        return "напиток";
+    } else if (number % 10 < 1 && number % 10 < 5) {
+        return "напитка";
+    }
+    return "напитков";
 }
